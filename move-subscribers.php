d78fce9ff4e580faea9bf2259eff91173f5b88f6
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2019-08-08 10:40:36 -0300

    Added error checking for email

--- move-subscribers.php
+++ move-subscribers.php
@@ -10,11 +10,13 @@ $MailChimp = new MailChimp($config['mailChimpAPI-Key']);
 
 $result = $MailChimp->get("lists/{$config['sourceListId']}/segments/{$config['sourceSegmentId']}/members");
 
-mail(
+if ( mail(
     $config['mailTo'],
     'Hay suscriptores listos para pasar a la lista general',
     "Se encontraron " . count($result['members']) . " listos para la lista general"
-);
+) == false ) {
+    echo 'Couldn\'t send email :('.PHP_EOL;
+}
 
 foreach ($result['members'] as $memberData) {
     echo 'Moving a member' . PHP_EOL;
