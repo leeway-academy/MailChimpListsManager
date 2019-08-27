0cbcfe945f19231f831b88b23f91a9f06897317d
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2019-08-27 15:47:47 -0300

    Actually moving members to general list

--- move-subscribers.php
+++ move-subscribers.php
@@ -10,18 +10,28 @@ $MailChimp = new MailChimp($config['mailChimpAPI-Key']);
 
 $result = $MailChimp->get("lists/{$config['sourceListId']}/segments/{$config['sourceSegmentId']}/members");
 
-if (count($result['members'])) {
-    if (mail(
-            $config['mailTo'],
-            'Hay suscriptores listos para pasar a la lista general',
-            "Se encontraron " . count($result['members']) . " listos para la lista general"
-        ) == false) {
-        echo 'Couldn\'t send email :(' . PHP_EOL;
+$members = $result['members'];
+if (count($members)) {
+    $added = 0;
+    foreach ($members as $member ) {
+        echo "Moving {$member['email_address']}".PHP_EOL;
+        $postURL = "lists/{$config['targetListId']}/members";
+        $postBody = [
+            'email_address' => $member['email_address'],
+            'merge_fields' => $member['merge_fields'],
+            'status' => $member['status'],
+        ];
+        $result = $MailChimp->post(
+            $postURL,
+            $postBody
+        );
+
+        $added += array_key_exists('id', $result ) ? 1 : 0;
+
+        $MailChimp->delete("lists/{$config['sourceListId']}/members/{$member['id']}");
     }
+
+    echo count($members)." subscribers found, $added moved to general list".PHP_EOL;
 } else {
     echo 'No members to be moved found'.PHP_EOL;
-}
-
-foreach ($result['members'] as $memberData) {
-    echo 'Moving a member' . PHP_EOL;
 }
